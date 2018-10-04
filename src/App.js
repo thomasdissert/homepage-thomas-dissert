747c436cebf03ca254210f9afc53c57a0e6597bb
Thomas Dissert <thomas.dissert@student.kit.edu>
2018-10-04 23:51:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Particles from 'react-particles-js';
+import "typeface-roboto"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <h1>
+        <span>T</span>homas <span>D</span>issert
+        </h1>
+        <Particles 
+        params={{
+          particles: {
+            number: {
+              value: 20,
+              density: {
+                enable: true,
+                value_area: 300
+              }
+            }
+
+          },
+          interactivity: {
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse"
+                }
+            }
+        }
+        }}
+        />
       </div>
-    );
+      );
   }
 }
 
