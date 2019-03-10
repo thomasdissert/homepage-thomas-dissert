090f33cb9fa3ca809541a7a355d108f3e2ad454c
Thomas Dissert <thomas.dissert@student.kit.edu>
2019-03-10 21:19:25 +0100

    new design

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import Header from './components/Header.js';
-import About from './components/About.js';
-import Work from './components/Work.js';
-import Footer from './components/Footer.js';
-
+import './App.css';
 import "typeface-roboto";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title="Thomas Dissert"/>
-        <About />
-        <Work />
-        <Footer author='Thomas Dissert' year='2019' email='info@thomasdissert.com' linkedin='https://www.linkedin.com/in/thomasdissert' xing='https://www.xing.com/profile/Thomas_Dissert'/>
+        
+        <div className="everything">
+          <div className="mainImage">
+            <img src={require('./images/name.png')} alt="thomasdissert" className="image"/>
+            <div className="impressum">
+              <a href="mailto:info@thomasdissert.com">&lt;Kontakt&gt;</a> &amp; <a href="mailto:info@thomasdissert.com">&lt;Impressum&gt;</a>
+            </div>
+          </div>
+          <div className="links">
+            <a href="https://www.twitch.tv/thomasdissert">
+              <img src={require('./images/twitch.png')} alt="Twitch" className="image"/>
+            </a>
+            <a href="https://www.youtube.com/channel/UCSjBSymr5mQk_D40DUyjAjg">
+              <img src={require('./images/youtube.png')} alt="Youtube" className="image"/>
+            </a>
+            <a href="https://www.twitter.com/thomasdissert">
+              <img src={require('./images/twitter.png')} alt="Twitter" className="image"/>
+            </a>
+            <a href="https://www.github.com/thomasdissert">
+              <img src={require('./images/github.png')} alt="Github" className="image"/>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
